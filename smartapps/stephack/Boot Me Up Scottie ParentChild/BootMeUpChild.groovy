53275b04c4c42fbdcc9f86139a427ade6245b5ca
Stephan Hackett <stephack@hotmail.com>
2018-10-18 14:51:55 -0400

    Create BootMeUpChild.groovy
    Child

---
+++ smartapps/stephack/Boot Me Up Scottie ParentChild/BootMeUpChild.groovy
@@ -0,0 +1,47 @@
+definition(
+    name: "Boot Me Up Scottie",
+    namespace: "stephack",
+    author: "Stephan Hackett",
+    description: "Activate WOL Magic Packet",
+    category: "Convenience",
+    parent: "stephack:Boot Me Up Manager",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "")
+
+preferences {    
+    section("Choose Switch") {
+    	input "myDevice", "capability.switch", required: true, title: "Choose a Switch"
+        input "myMac", "text", required: true, title: "MAC of workstation"
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	subscribe(myDevice, "switch.on", myHandler)
+}
+
+def myHandler(evt) {
+	log.info "${myDevice} activated"
+    sendHubCommand(createWOL())
+}
+
+def createWOL(evt) {
+    log.debug "Sending Magic Packet to: $myMac"
+    def result = new hubitat.device.HubAction (
+       	"wake on lan $myMac",
+       	hubitat.device.Protocol.LAN,
+       	null
+    )    
+    return result
+}
