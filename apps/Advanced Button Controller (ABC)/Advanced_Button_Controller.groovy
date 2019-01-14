9098209d6222c45edf0f694e20d6fb533dca6768
Stephan Hackett <stephack@hotmail.com>
2019-01-14 16:13:49 -0500

    01/14/19
    - added url to Raw code at the top of the parent/child apps
    - adjusted logging
    - update checking code is now done through json file (Thanks to @Cobra for his guidance)

--- apps/Advanced Button Controller (ABC)/Advanced_Button_Controller.groovy
+++ apps/Advanced Button Controller (ABC)/Advanced_Button_Controller.groovy
@@ -1,11 +1,20 @@
 /*
+ *	Hubitat Import URL: https://raw.githubusercontent.com/stephack/Hubitat/master/apps/Advanced%20Button%20Controller%20(ABC)/Advanced_Button_Controller.groovy
+ *
+ *
  *	Advanced Button Controller (Parent/Child Version)
  *
  *	Author: Stephan Hackett
  * 
- * 7/03/18 - added pictures and Update check
+ *	
+ *	01/14/19 - added url to Raw code at the top of the parent/child apps
+ *			 - adjusted logging
+ *			 - update checking code is now done through json file (Thanks to @Cobra for his guidance)
+ *
+ *	07/03/18 - added pictures and Update check
  * 
  */
+def version(){"v0.2.190114"}
 
 definition(
     name: "Advanced Button Controller",
@@ -14,62 +23,43 @@ definition(
     author: "Stephan Hackett",
     description: "Configure devices with buttons like the Aeon Labs Minimote and Lutron Pico Remotes.",
     category: "My Apps",
-    iconUrl: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abcNew.png",
-    iconX2Url: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abcNew.png",
-    iconX3Url: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abcNew.png",
+    iconUrl: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abc2.png",
+    iconX2Url: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abc2.png",
+    iconX3Url: "https://cdn.rawgit.com/stephack/ABC/master/resources/images/abc2.png",
 )
 
 preferences {
 	page(name: "mainPage")
     page(name: "aboutPage", nextPage: "mainPage")
-	
 }
 
 def mainPage() {
 	return dynamicPage(name: "mainPage", title: "", install: true, uninstall: true) {
         if(!state.abcInstalled) {
             section("Hit Done to install ABC App!") {
-        }
-            
+        	}
         }
         else {
-    	def childApps = getAllChildApps()
-        def childVer = "Initial Setup - Version Unknown"
-        if(childApps.size() > 0) {
-        	childVer = childApps.first().version()
-        }
-        section("Create a new button device mapping.") {
-            app(name: "childApps", appName: "ABC Button Mapping", namespace: "stephack", title: "<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/New.png height=50 width=50>      New Button Device Mapping", multiple: true)
-        }
-        section("Version Info, User's Guide") {
-            href (name: "aboutPage", title: "${verImgCheck(childVer)}   Advanced Button Controller \n"+childVer, 
-       		description: "Tap to get Smartapp Info and User's Guide.",
-       		//image: verImgCheck(childVer), required: false, // check repo for image that matches current version. Displays update icon if missing
-       		page: "aboutPage"
-		)		
-   		}
-        //remove("Uninstall ABC App","WARNING!!","This will remove the ENTIRE SmartApp, including all configs listed above.")
+    		def childApps = getAllChildApps()
+        	def childVer = "Initial Setup - Version Unknown"
+        	if(childApps.size() > 0) {
+        		childVer = childApps.first().version()
+        	}
+			section(){
+				def appHead = "<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/abc2.png height=80 width=80> \n${checkForUpdate()}"
+				paragraph "<div style='text-align:center'>${appHead}</div>"
+			}
+        	section("Create a new button device mapping.") {
+            	app(name: "childApps", appName: "ABC Button Mapping", namespace: "stephack", title: "<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/New.png height=50 width=50>      New Button Device Mapping", multiple: true)
+        	}
+			section("") {
+				href (name: "aboutPage", title: "User's Guide", 
+       				description: "",
+       				page: "aboutPage"
+				)		
+   			}
         }
     }
-}
-
-def verImgCheck(childVer){
-	def params = [
-    	uri: "https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/abc_${childVer}.png",
-	]
-	try {
-   		httpGet(params) { resp ->
-        	resp.headers.each {
-           	//log.debug "${it.name} : ${it.value}"
-        	}
-            log.debug "ABC appears to be running the latest Version"
-            log.info params
-            return "<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/abc_${childVer}.png height=50 width=50>"//params.uri//"<img src=${params.uri} height=80 width=80>"
-    	}
-	} catch (e) {
-    	log.error "ABC does not appear to be the latest version: Please update from IDE"
-    	return "<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/update.png height=50 width=50>"
-	}
 }
 
 def installed() {
@@ -83,7 +73,28 @@ def updated() {
 }
 
 def initialize() {
+}
 
+def checkForUpdate(){
+	def params = [uri: "https://raw.githubusercontent.com/stephack/Hubitat/master/apps/Advanced%20Button%20Controller%20(ABC)/parent.json",
+				   	contentType: "application/json"]
+       	try {
+			httpGet(params) { response ->
+				def results = response.data
+				def appStatus
+				if(version() == results.currVersion){
+					appStatus = "<small>Parent ${version()}</small><br>${results.noUpdateImg}"
+				}
+				else {
+					appStatus = "<small>Parent ${version()}</small><br>${results.updateImg}${results.changeLog}"
+					log.warn "ABC Parent App does not appear to be the latest version: Please update."
+				}
+				return appStatus
+			}
+		} 
+        catch (e) {
+        	log.error "Error:  $e"
+    	}
 }
 
 def aboutPage() {
@@ -94,7 +105,7 @@ def aboutPage() {
 
 private def textHelp() {
 	def text =
-	section("<img src=https://raw.githubusercontent.com/stephack/ABC/master/resources/images/abcNew.png height=36 width=36> User's Guide - Advanced Button Controller") {
+	section("<img src=https://raw.githubusercontent.com/stephack/Hubitat/master/resources/images/abc2.png height=36 width=36> User's Guide - Advanced Button Controller") {
     	paragraph "This smartapp allows you to use a device with buttons including, but not limited to:\n\n  Aeon Labs Minimotes\n"+
     	"  HomeSeer HS-WD100+ switches**\n  HomeSeer HS-WS100+ switches\n  Lutron Picos***\n\n"+
 		"It is a heavily modified version of @dalec's 'Button Controller Plus' which is in turn"+
@@ -110,36 +121,40 @@ private def textHelp() {
     	"(***Standard Pico remotes can be converted to Audio Picos)\n\nThe additional control options have been highlighted below."
 	}
 	section("Available Control Options are:"){
-        paragraph "	Switches - Toggle \n"+
-        "	Switches - Turn On \n"+
+        paragraph "	Switches - Turn On \n"+
         "	Switches - Turn Off \n"+
-        "	Switches - Toggle \n"+
+        "	Switches - Toggle \n\n"+
             
         "	Dimmers - Set Level (Group 1) \n"+
         "	Dimmers - Set Level (Group 2) \n"+
-        "	*Dimmers - Inc Level \n"+
-        "	*Dimmers - Dec Level \n"+
-        "	*Dimmers - Toggle on to Level \n"+
-        "	*Dimmers - Ramp Up/Down (Smooth Dimming) \n"+
+        "	Dimmers - Inc Level \n"+
+        "	Dimmers - Dec Level \n"+
+        "	Dimmers - Toggle on to Level \n"+
+        "	Dimmers - Ramp Up/Down (Smooth Dimming) \n\n"+
             
-        "	*Color Lights - Set Temperature \n"+
-        "	*Color Lights - Set Color \n"+
+        "	Color Lights - Set Temperature \n"+
+        "	Color Lights - Set Color \n\n"+
         
-        "	*Speaker - Toggle Play/Pause \n"+
-        "	*Speaker - Increment Volume \n"+
-        "	*Speaker - Decrement Volume \n"+
-        "	*Speaker - Next Track \n"+
-        "	*Speaker - Mute/Unmute \n"+
-        "	*Speaker - Cycle Preset \n"+
+        "	Speaker - Toggle Play/Pause \n"+
+        "	Speaker - Increment Volume \n"+
+        "	Speaker - Decrement Volume \n"+
+        "	Speaker - Next Track \n"+
+        "	Speaker - Mute/Unmute \n"+
+        "	Speaker - Cycle Preset \n\n"+
             
          "	Fans - Set Speed \n"+
         "	Fans - Cycle Speed \n"+    
-        "	Fans - Legacy Cycle (Low, Medium, High, Off) \n"+  
+        "	Fans - Legacy Cycle (Low, Medium, High, Off) \n\n"+  
+			
+		"	Modes - Set Mode \n"+
+		"	Modes - Set HSM \n\n"+	
+			
+		"	Rules - Run, Stop, Pause, Resume, Evaluate, Set Boolean \n\n"+	
             
         "	Locks - Unlock Only \n"+
+		"	Scenes - Cycle Scenes \n"+	
         "	Shades - Up, Down, or Stop \n"+
         "	Sirens - Toggle \n"+
-        "	Set Modes \n"+
         "	Speech Notifications \n"+
         "	SMS Notifications"
 	}
@@ -154,6 +169,6 @@ private def textHelp() {
 	}
 	section("*** Lutron Pico:"){
         paragraph "There are 2 types of Pico configurations in HE:\n 1. The Standard Picos - with pushed events and held events (followed by released events).\n"+
-    	"2. The Fast Picos - with pushed events followed by released events (no held events)."
+    	" 2. The Fast Picos - with pushed events followed by released events (no held events)."
 	}
 }
