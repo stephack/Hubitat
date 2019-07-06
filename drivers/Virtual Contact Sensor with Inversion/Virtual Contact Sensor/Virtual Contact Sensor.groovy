68b16d1038e420bb889df0e7a7f508a193b7c857
Stephan Hackett <20565645+stephack@users.noreply.github.com>
2019-07-06 08:53:02 -0400

    Updated copyright year

--- drivers/Virtual Contact Sensor with Inversion/Virtual Contact Sensor/Virtual Contact Sensor.groovy
+++ drivers/Virtual Contact Sensor with Inversion/Virtual Contact Sensor/Virtual Contact Sensor.groovy
@@ -2,7 +2,7 @@
 *  Virtual Contact Sensor
 *
 *
-*  Copyright 2018 Stephan Hackett
+*  Copyright 2019 Stephan Hackett
 *
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 *  in compliance with the License. You may obtain a copy of the License at:
